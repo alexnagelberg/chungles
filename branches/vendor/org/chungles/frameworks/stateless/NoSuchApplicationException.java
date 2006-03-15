0cd5a3319b0cb4e500d596beb91a651d73ea7c58
unknown <unknown@localhost>
2006-03-15 23:43:10 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.

---
+++ branches/vendor/org/chungles/frameworks/stateless/NoSuchApplicationException.java
@@ -0,0 +1,8 @@
+package org.chungles.frameworks.stateless;
+
+public class NoSuchApplicationException extends Exception
+{
+	// Eclipse generates this :)
+	private static final long serialVersionUID = 7129523347026857156L;
+	
+}

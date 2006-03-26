b0a29d2616e7250c7967529ae7956ab8c66d9b71
alex <1258532+alexnagelberg@users.noreply.github.com>
2006-03-26 08:44:34 +0000

    cleanup

--- branches/vendor/org/chungles/frameworks/stateless/ApplicationAlreadyRegisteredException.java
+++
@@ -1,8 +0,0 @@
-package org.chungles.frameworks.stateless;
-
-public class ApplicationAlreadyRegisteredException extends Exception
-{
-    // Eclipse generates this :)
-    private static final long serialVersionUID = 8546205517136886565L;
-
-}
--- branches/vendor/org/chungles/frameworks/stateless/State.java
+++
@@ -1,9 +0,0 @@
-package org.chungles.frameworks.stateless;
-
-import java.io.Serializable;
-
-public interface State extends Serializable
-{	
-    // returns amount of memory state needs
-    public long requiredMemory();	
-}

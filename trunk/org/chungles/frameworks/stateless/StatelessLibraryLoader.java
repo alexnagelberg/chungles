d98bfeeac95a8e919218e1b241bf3aff75d313ae
alex <1258532+alexnagelberg@users.noreply.github.com>
2006-03-27 23:36:51 +0000

    Added java library loading using simontuff's one-jar (also fixed line in one-jar)

--- trunk/org/chungles/frameworks/stateless/StatelessLibraryLoader.java
+++ trunk/org/chungles/frameworks/stateless/StatelessLibraryLoader.java
@@ -1,6 +1,36 @@
 package org.chungles.frameworks.stateless;
 
+import com.simontuffs.onejar.*;
+import java.io.*;
+import java.util.jar.*;
+
 public class StatelessLibraryLoader
 {
-    // Loads a java library in to the framework
+    /**
+     * Loads stateless app
+     * 
+     * @param path Path to stateless Jar
+     * @return new StatelessApplication object containing app
+     */
+	public static StatelessApplication addApplication(String path)
+		throws BadApplicationException
+	{
+		try
+		{
+			FileInputStream fin=new FileInputStream(path);
+			JarInputStream jin=new JarInputStream(fin);
+			Manifest m=jin.getManifest();
+			Attributes attr=m.getMainAttributes();
+			String mainClass=attr.getValue("Stateless-Class");
+			jin.close();
+			JarClassLoader loader=new JarClassLoader(ClassLoader.getSystemClassLoader());
+			loader.load(mainClass, path);
+			StatelessApplication app=(StatelessApplication)(loader.loadClass("Test").newInstance());
+			return app;
+		}
+		catch (Exception e)
+		{
+			throw (BadApplicationException)e;
+		}
+	}
 }

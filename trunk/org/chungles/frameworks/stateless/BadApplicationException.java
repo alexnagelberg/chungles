3767ac68eaca7ea95541c870e02a6321191aa3fc
alex <alex@localhost>
2006-03-28 00:10:41 +0000

    Exception thrown when class tries to load a bad stateless app

---
+++ trunk/org/chungles/frameworks/stateless/BadApplicationException.java
@@ -0,0 +1,7 @@
+package org.chungles.frameworks.stateless;
+
+public class BadApplicationException extends Exception
+{
+	// Eclipse generates this :)
+	private static final long serialVersionUID = 3100653586409684886L;
+}

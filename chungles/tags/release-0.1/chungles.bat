d1f0e10a81438368258ec1169453e28693c694e8
rammerhammer <1258532+alexnagelberg@users.noreply.github.com>
2006-04-20 00:02:54 +0000

    whoops, no jsap

--- chungles/tags/release-0.1/chungles.bat
+++ chungles/tags/release-0.1/chungles.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=lib -classpath lib\chungles.jar;lib\JSAP_1.03a.jar;lib\jmdns.jar;lib\swt.jar Main
+java -Djava.library.path=lib -classpath lib\chungles.jar;lib\jmdns.jar;lib\swt.jar Main

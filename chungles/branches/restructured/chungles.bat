f95679508e8b1af946d82c3b7737f015f1cb93a3
rammerhammer <rammerhammer@localhost>
2006-04-28 20:50:02 +0000

    updated for restructured code

--- chungles/branches/restructured/chungles.bat
+++ chungles/branches/restructured/chungles.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=lib -classpath lib\chungles.jar;lib\jmdns.jar;lib\swt.jar Main
+java -Djava.library.path=lib -classpath lib\chungles.jar;lib\jmdns.jar;lib\swt.jar org.chungles.application.Main

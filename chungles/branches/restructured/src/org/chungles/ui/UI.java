63b48ddf5ed43143b096798f805525d608fba1a7
rammerhammer <rammerhammer@localhost>
2006-09-14 19:16:03 +0000

    Hey, some javadoc!

--- chungles/branches/restructured/src/org/chungles/ui/UI.java
+++ chungles/branches/restructured/src/org/chungles/ui/UI.java
@@ -4,10 +4,37 @@ import java.util.*;
 
 public interface UI
 {
+	/**
+	 * This is the UI's main loop. This method should be blocking. 
+	 *
+	 */
 	public void takeover();
 	
+	/**
+	 * Called when a new node is discovered. Sometimes sends duplicates so checking
+	 * for so must be done.
+	 *  
+	 * @param IP IP address of node discovered
+	 * @param compname Node discovered's name
+	 * @param ips Hash table of IPs with computer name for keys
+	 * @param compnames Hash table of computer names with IPs for keys
+	 */
 	public void addNode(String IP, String compname, Hashtable ips, Hashtable compnames);
+	
+	/**
+	 * Called when a node is no longer on the network. Sometimes sends duplicates so
+	 * checking for so must be done.
+	 * 
+	 * @param IP IP address of node offlined
+	 * @param compname Offlined node's name
+	 * @param ips Hash table of IPs with computer name for keys
+	 * @param compnames Hash table of computer names with IPs for keys
+	 */
 	public void removeNode(String IP, String compname, Hashtable ips, Hashtable compnames);
 	
+	/**
+	 * Should open a preferences dialog
+	 *
+	 */
 	public void openPreferencesDialog();
 }

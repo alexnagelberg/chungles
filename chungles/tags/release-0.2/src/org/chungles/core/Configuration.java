5a99de290a688476319550c7828d4f29b94e88f6
rammerhammer <rammerhammer@localhost>
2006-11-03 22:17:43 +0000

    Tagging 0.2 release

---
+++ chungles/tags/release-0.2/src/org/chungles/core/Configuration.java
@@ -0,0 +1,51 @@
+package org.chungles.core;
+
+import java.util.*;
+
+public class Configuration
+{
+	private static Hashtable<String, String> shares;
+	private static String computerName;
+	private static String REV=" ";
+	
+	public static void init()
+	{
+		shares=new Hashtable<String, String>();
+		computerName="Chungles Node";
+	}
+	
+	public static String getVersion()
+	{
+		return "r"+REV;
+	}
+	
+	public static String getSharePath(String name)
+	{
+		return shares.get(name);
+	}
+	
+	public static Iterator<String> getSharesIterator()
+	{
+		return shares.keySet().iterator();
+	}
+	
+	public static void addShare(String name, String path)
+	{
+		shares.put(name, path);
+	}
+	
+	public static void clearShares()
+	{
+		shares.clear();
+	}
+	
+	public static String getComputerName()
+	{
+	    return computerName;
+	}
+	
+	public static void setComputerName(String name)
+	{
+	    computerName=name;
+	}
+}
---
+++ chungles/tags/release-0.2/src/org/chungles/core/ServerThread.java
@@ -0,0 +1,38 @@
+package org.chungles.core;
+
+import java.io.IOException;
+import java.net.*;
+
+public class ServerThread extends Thread
+{
+	private boolean listening = true;
+	
+	public ServerThread()
+	{
+		super("ServerThread");
+	}
+	
+	public void stopListening()
+	{
+		listening=false;
+	}
+	
+	public void run()
+	{
+		ServerSocket serverSocket = null;
+	    	
+	    try
+		{
+	        serverSocket = new ServerSocket(6565);
+	        while (listening)
+		    {
+		    	new ServerConnectionThread(serverSocket.accept()).start();
+		    }
+		    serverSocket.close();
+	    }
+	    catch (IOException e)
+		{
+	        e.printStackTrace();
+	    }	    
+	}
+}

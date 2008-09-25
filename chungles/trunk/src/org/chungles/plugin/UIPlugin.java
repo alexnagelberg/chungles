636ed5dd17a2d49598478b8bd0f69d7d4a0c3816
rammerhammer <1258532+alexnagelberg@users.noreply.github.com>
2008-09-25 21:14:58 +0000

    Replaced poll and sleep method of determining if UI is finished and replaced with placing main thread in wait state, plugin can interrupt thread to shutdown.

--- chungles/trunk/src/org/chungles/plugin/UIPlugin.java
+++ chungles/trunk/src/org/chungles/plugin/UIPlugin.java
@@ -6,5 +6,4 @@ public interface UIPlugin extends StandardPlugin
     public void removeNode(String IP, String compname);
     public void openPreferencesDialog();
     public void finishnotification(boolean success, String message);
-    public boolean isDone();
 }
